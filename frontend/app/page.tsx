ba52edc16e2e2bf57785d9e1b0f0d3d7f8ff1524
RashMin <rashmin9c@gmail.com>
2024-03-04 23:11:17 +0530

    hmmnn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
 import Articles from "@/components/articles";
 import Steps from "@/components/steps"
-import Team from "@/components/ourteam";
 import Footer from "@/components/footer";
 
 export default function Home(): JSX.Element {
