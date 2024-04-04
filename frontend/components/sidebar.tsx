52062fda307c8b78f7511562ecfb2f2d6ee4008e
Swayam Takkamore <70209059+SwayamTakkamore@users.noreply.github.com>
2024-04-04 22:28:53 +0530

    updated Project Name in sidebar

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -30,7 +30,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
   return (
     <div ref={sidebarRef} className={`fixed inset-y-0 left-0 z-50 bg-white w-64 shadow-xl transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition duration-300 ease-in-out`}>
       <div onClick={toggleSidebar} className="flex items-center justify-between px-4 py-6 border-b">
-        <h2 className="text-lg font-semibold">Jijivisha</h2>
+        <h2 className="text-lg font-semibold">Eunoia</h2>
         <button className="text-gray-500 focus:outline-none">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
             <path d="M6 18L18 6M6 6l12 12"></path>
